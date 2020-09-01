7d8020c17f0904b2f675f64226da620c0b2f9ba3
duggabe <barry@dcsmail.net>
2020-09-01 15:28:07 -0500

    initial commit

---
+++ RTTY_rcv/RTTY_receive.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: RTTY_receive
+# Author: Barry Duggan
+# GNU Radio version: 3.9.0.0-git
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from PyQt5.QtCore import QObject, pyqtSlot
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import analog
+import math
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import filter
+from gnuradio import gr
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import zeromq
+import epy_block_0
+
+from gnuradio import qtgui
+
+class RTTY_receive(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "RTTY_receive", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("RTTY_receive")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "RTTY_receive")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 48000
+        self.reverse = reverse = 1
+        self.fsk_deviation = fsk_deviation = 170
+        self.center = center = 2210
+        self.baud = baud = 1/0.022
+
+        ##################################################
+        # Blocks
+        ##################################################
+        # Create the options list
+        self._reverse_options = (1, -1, )
+        # Create the labels list
+        self._reverse_labels = ('Normal', 'Reverse', )
+        # Create the combo box
+        # Create the radio buttons
+        self._reverse_group_box = Qt.QGroupBox('reverse' + ": ")
+        self._reverse_box = Qt.QVBoxLayout()
+        class variable_chooser_button_group(Qt.QButtonGroup):
+            def __init__(self, parent=None):
+                Qt.QButtonGroup.__init__(self, parent)
+            @pyqtSlot(int)
+            def updateButtonChecked(self, button_id):
+                self.button(button_id).setChecked(True)
+        self._reverse_button_group = variable_chooser_button_group()
+        self._reverse_group_box.setLayout(self._reverse_box)
+        for i, _label in enumerate(self._reverse_labels):
+            radio_button = Qt.QRadioButton(_label)
+            self._reverse_box.addWidget(radio_button)
+            self._reverse_button_group.addButton(radio_button, i)
+        self._reverse_callback = lambda i: Qt.QMetaObject.invokeMethod(self._reverse_button_group, "updateButtonChecked", Qt.Q_ARG("int", self._reverse_options.index(i)))
+        self._reverse_callback(self.reverse)
+        self._reverse_button_group.buttonClicked[int].connect(
+            lambda i: self.set_reverse(self._reverse_options[i]))
+        self.top_grid_layout.addWidget(self._reverse_group_box)
+        self.zeromq_push_msg_sink_0 = zeromq.push_msg_sink('tcp://127.0.0.1:50252', 100, True)
+        self.rational_resampler_xxx_0_0 = filter.rational_resampler_fff(
+                interpolation=500,
+                decimation=960,
+                taps=None,
+                fractional_bw=None)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            128, #size
+            500, #samp_rate
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 2)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_NORM, qtgui.TRIG_SLOPE_NEG, 0.5, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_fcf(50, firdes.low_pass(1.0,samp_rate,1000,400), center, samp_rate)
+        self.epy_block_0 = epy_block_0.my_sync_block()
+        self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(reverse)
+        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
+        self.audio_source_0 = audio.source(samp_rate, '', True)
+        self.analog_quadrature_demod_cf_1 = analog.quadrature_demod_cf(samp_rate/50*(2*math.pi*fsk_deviation/8.0))
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.epy_block_0, 'msg_out'), (self.zeromq_push_msg_sink_0, 'in'))
+        self.connect((self.analog_quadrature_demod_cf_1, 0), (self.rational_resampler_xxx_0_0, 0))
+        self.connect((self.audio_source_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
+        self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.digital_binary_slicer_fb_0, 0))
+        self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_char_to_float_0, 0))
+        self.connect((self.digital_binary_slicer_fb_0, 0), (self.epy_block_0, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_quadrature_demod_cf_1, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "RTTY_receive")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_quadrature_demod_cf_1.set_gain(self.samp_rate/50*(2*math.pi*self.fsk_deviation/8.0))
+        self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(1.0,self.samp_rate,1000,400))
+
+    def get_reverse(self):
+        return self.reverse
+
+    def set_reverse(self, reverse):
+        self.reverse = reverse
+        self._reverse_callback(self.reverse)
+        self.blocks_multiply_const_vxx_0.set_k(self.reverse)
+
+    def get_fsk_deviation(self):
+        return self.fsk_deviation
+
+    def set_fsk_deviation(self, fsk_deviation):
+        self.fsk_deviation = fsk_deviation
+        self.analog_quadrature_demod_cf_1.set_gain(self.samp_rate/50*(2*math.pi*self.fsk_deviation/8.0))
+
+    def get_center(self):
+        return self.center
+
+    def set_center(self, center):
+        self.center = center
+        self.freq_xlating_fir_filter_xxx_0.set_center_freq(self.center)
+
+    def get_baud(self):
+        return self.baud
+
+    def set_baud(self, baud):
+        self.baud = baud
+
+
+
+
+
+def main(top_block_cls=RTTY_receive, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
---
+++ RTTY_xmt/RTTY_vco.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: RTTY_vco
+# Author: Barry Duggan
+# Description: RTTY transmitter
+# GNU Radio version: 3.9.0.0-git
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import filter
+from gnuradio import gr
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import zeromq
+import epy_block_0_0
+
+from gnuradio import qtgui
+
+class RTTY_vco(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "RTTY_vco", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("RTTY_vco")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "RTTY_vco")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.vco_max = vco_max = 2500
+        self.fsk_deviation = fsk_deviation = 170
+        self.center = center = 2210
+        self.vco_offset = vco_offset = (center-(fsk_deviation/2))/vco_max
+        self.samp_rate = samp_rate = 48000
+        self.repeat = repeat = (int)(samp_rate*0.022)
+        self.inp_amp = inp_amp = ((center+(fsk_deviation/2))/vco_max)-vco_offset
+        self.baud = baud = 1/0.022
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.zeromq_pull_msg_source_0 = zeromq.pull_msg_source('tcp://127.0.0.1:50251', 100, False)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            4096, #size
+            samp_rate, #samp_rate
+            "", #name
+            2, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1.5)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_NORM, qtgui.TRIG_SLOPE_NEG, 0.5, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.low_pass_filter_0 = filter.fir_filter_fff(
+            1,
+            firdes.low_pass(
+                1,
+                samp_rate,
+                200,
+                1000,
+                firdes.WIN_HAMMING,
+                6.76))
+        self.epy_block_0_0 = epy_block_0_0.mc_sync_block()
+        self.blocks_vco_f_0 = blocks.vco_f(samp_rate, 15708, 0.5)
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, repeat)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(inp_amp)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff(vco_offset)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.zeromq_pull_msg_source_0, 'out'), (self.epy_block_0_0, 'msg_in'))
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_vco_f_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_uchar_to_float_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_vco_f_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.blocks_vco_f_0, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.epy_block_0_0, 0), (self.blocks_repeat_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.qtgui_time_sink_x_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "RTTY_vco")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_vco_max(self):
+        return self.vco_max
+
+    def set_vco_max(self, vco_max):
+        self.vco_max = vco_max
+        self.set_inp_amp(((self.center+(self.fsk_deviation/2))/self.vco_max)-self.vco_offset)
+        self.set_vco_offset((self.center-(self.fsk_deviation/2))/self.vco_max)
+
+    def get_fsk_deviation(self):
+        return self.fsk_deviation
+
+    def set_fsk_deviation(self, fsk_deviation):
+        self.fsk_deviation = fsk_deviation
+        self.set_inp_amp(((self.center+(self.fsk_deviation/2))/self.vco_max)-self.vco_offset)
+        self.set_vco_offset((self.center-(self.fsk_deviation/2))/self.vco_max)
+
+    def get_center(self):
+        return self.center
+
+    def set_center(self, center):
+        self.center = center
+        self.set_inp_amp(((self.center+(self.fsk_deviation/2))/self.vco_max)-self.vco_offset)
+        self.set_vco_offset((self.center-(self.fsk_deviation/2))/self.vco_max)
+
+    def get_vco_offset(self):
+        return self.vco_offset
+
+    def set_vco_offset(self, vco_offset):
+        self.vco_offset = vco_offset
+        self.set_inp_amp(((self.center+(self.fsk_deviation/2))/self.vco_max)-self.vco_offset)
+        self.blocks_add_const_vxx_0.set_k(self.vco_offset)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_repeat((int)(self.samp_rate*0.022))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 200, 1000, firdes.WIN_HAMMING, 6.76))
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+
+    def get_repeat(self):
+        return self.repeat
+
+    def set_repeat(self, repeat):
+        self.repeat = repeat
+        self.blocks_repeat_0.set_interpolation(self.repeat)
+
+    def get_inp_amp(self):
+        return self.inp_amp
+
+    def set_inp_amp(self, inp_amp):
+        self.inp_amp = inp_amp
+        self.blocks_multiply_const_vxx_0.set_k(self.inp_amp)
+
+    def get_baud(self):
+        return self.baud
+
+    def set_baud(self, baud):
+        self.baud = baud
+
+
+
+
+
+def main(top_block_cls=RTTY_vco, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
